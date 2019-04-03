602c72af44bfdd0b61608d4068d389610fead682
Rafael Vargas <vargas718@gmail.com>
2019-04-03 15:06:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,77 @@
+require "pry"
+
+
 class Owner
-  # code goes here
+  @@all = []
+   
+  attr_accessor :owner, :name, :pets 
+  attr_reader :species 
+  
+   
+  
+  
+ 
+  def initialize (species, owner = "human")
+    @species = species
+    @owner = owner 
+    @@all << self
+    @pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+    #binding.pry
+  end 
+ 
+  def self.all
+  
+    @@all
+  #binding.pry
+  end 
+  
+  def self.count 
+    @@all.size 
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end 
+  
+  def say_species
+   "I am a human."
+  end 
+  
+  def buy_fish(name) 
+    pets[:fishes] << Fish.new(name)
+    
+  # binding.pry 
+    
+  end 
+  
+   def buy_cat(name) 
+    pets[:cats] << Cat.new(name)
+    
+  # binding.pry 
+    
+   end 
+  
+   def buy_dog(name) 
+    pets[:dogs] << Dog.new(name)
+    
+  # binding.pry 
+    
+   end 
+   
+   def walk_dogs
+       pets[:dogs].each{|dog| dog.mood= "happy"}
+   end 
+   
+   def play_with_cats 
+      pets[:cats].each{|cat| cat.mood= "happy"}
+   end 
+  
+   def feed_fish
+   pets[:fishes].each{|fish| fish.mood= "happy"}
+   end 
+   
+   def sell_pets 
+     pets.each {|name, mood| name}
+     binding.pry
+   end 
 end
